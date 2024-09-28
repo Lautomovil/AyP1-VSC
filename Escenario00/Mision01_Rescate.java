94f4eb073a051b88acd47f79f02ff2529b89c0f6
MaxLam99 <maxi-pequi99@hotmail.com.ar>
2024-09-28 03:09:20 -0300

    Mapa de la Misión Rescate Creado y Definido

---
+++ Escenario00/Mision01_Rescate.java
@@ -0,0 +1,104 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Mision01_Rescate extends MundoBase
+{
+            
+    public Mision01_Rescate()
+    {
+        super(7, 8, 100);
+        inicializarPosicionesOcupadas();
+    }
+        
+    private void inicializarPosicionesOcupadas(){ // Esto ya marca las posiciones predeterminadas
+        // Añade las posiciones ocupadas por asteroides que rodean al piloto
+        marcarPosicionOcupada(2, 2);
+        marcarPosicionOcupada(3, 2);
+        marcarPosicionOcupada(4, 2);
+        marcarPosicionOcupada(2, 0);
+        marcarPosicionOcupada(3, 0);
+        marcarPosicionOcupada(4, 0);
+        marcarPosicionOcupada(2, 1);
+        marcarPosicionOcupada(4, 1);
+        
+        // Añade la posición de la base
+        marcarPosicionOcupada(0, 7);
+        
+        // Añade la posición del piloto perdido
+        marcarPosicionOcupada(3, 1);
+        
+        // Añade la posición de la nave exploradora
+        marcarPosicionOcupada(0, 7);
+    }
+    
+    private void marcarPosicionOcupada(int x, int y) {
+        // Marca como ocupadas las posiciones en X e Y
+        posicionesOcupadas[x][y] = true;
+    }
+    
+    private boolean esPosicionLibre(int x, int y) {
+        // Esto dice si el recuadro está libre o no
+        return !posicionesOcupadas[x][y];
+    }
+    
+    protected void generarAsteroides() {
+        // Asteroides que rodean al piloto
+        agregar(new Asteroide(), 2, 2);
+        agregar(new Asteroide(), 3, 2);
+        agregar(new Asteroide(), 4, 2);
+        agregar(new Asteroide(), 2, 0);
+        agregar(new Asteroide(), 3, 0);
+        agregar(new Asteroide(), 4, 0);
+        agregar(new Asteroide(), 2, 1);
+        agregar(new Asteroide(), 4, 1);
+                
+        // Marcar estas posiciones como ocupadas
+        inicializarPosicionesOcupadas();
+        
+        // Asteroides generados aleatoriamente cada vez que inicia el mundo
+        generarAsteroidesAleatorios(5); // Genera 5 asteroides aleatorios (Podemos cambiar el número)
+    }
+    
+    private void generarAsteroidesAleatorios(int cantidad) {
+        int x, y;
+        for (int i = 0; i < cantidad; i++) {
+            do {
+                x = (int) (Math.random() * getWidth()); 
+                y = (int) (Math.random() * getHeight()); 
+            } while (!esPosicionLibre(x, y));
+            agregar(new Asteroide(), x, y);
+            marcarPosicionOcupada(x, y); // Marca la posición como ocupada
+        }
+        /*
+         * getWidth y getHeight son métodos propios de Greenfoot, en este caso nos darían coordenadas
+         * random para x e y con Math.random si el espacio no está ocupado
+         */
+    }
+    
+    protected void generarPOIs() {
+        // Base al borde inferior izquierdo del mapa
+        marcarCelda(0, 7, new Color(0, 0, 200, 150));
+    }
+    
+    protected void generarNaves() {
+        // Nave Exploradora generada en la base
+        agregar(new NaveExploradora(), 0, 7);
+        
+        // Piloto Perdido en el campo de asteroides (Después cambiar al tipo de piloto)
+        agregar(new PilotoDeEjemplo(), 3, 1);
+    }
+    
+     protected void generarItems() {
+        // Placeholder para la estrella fugaz
+        generarEstrella(new Item());
+    }
+    
+    private void generarEstrella(Item item) {
+        int x, y;
+        do {
+            x = (int) (Math.random() * getWidth());
+            y = (int) (Math.random() * getHeight());
+        } while (!esPosicionLibre(x, y));
+        agregar(item, x, y);
+        marcarPosicionOcupada(x, y);
+    }
+}
